e119c55e7c32d3062bc1be379f9d5f549f9ec78f
David Hobley <david.hobley@sharpblue.com.au>
2025-07-14 11:07:05 +1000

    fixed issue if we have books, but not started reading one as yet

---
+++ lib/widgets/home/initial_instructions.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:paladin/repositories/books_repository.dart';
+
+import '../../screens/calibresync.dart';
+import '../menu/paladin_menu.dart';
+import 'fatal_error.dart';
+
+class InitialInstructions extends ConsumerWidget {
+  const InitialInstructions({super.key,});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    var bookCountAsync = ref.watch(booksRepositoryProvider);
+
+    return bookCountAsync.when(error: (error, stackTrace) {
+      return FatalError(error: error.toString(), trace: stackTrace);
+    }, loading: () {
+    return const Center(child: CircularProgressIndicator());
+    }, data: (int count) {
+      return Expanded(
+        child: Stack(
+          children: [
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 5.0, bottom: 2.0),
+                child: count == 0
+                    ? TextButton(
+                  onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => CalibreSync())),
+                  style: const ButtonStyle(backgroundColor: WidgetStatePropertyAll(Colors.white), foregroundColor: WidgetStatePropertyAll(Colors.black)),
+                  child: Text(
+                    'Synchronise Library',
+                    textAlign: TextAlign.center,
+                    style: Theme
+                        .of(context)
+                        .textTheme
+                        .labelSmall,
+                  ),
+                )
+                    : Text('Congratulations on your new eReader. Pick a book. Enjoy!', textAlign: TextAlign.center, style: Theme
+                    .of(context)
+                    .textTheme
+                    .labelSmall),
+              ),
+            ),
+            Align(
+              alignment: Alignment.topRight,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  PaladinMenu(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      );
+    });
+  }
+}
