4ea1b6fa951ac109347eb871c3fdaf1aef9ee221
David Hobley <david.hobley@sharpblue.com.au>
2025-07-27 09:49:49 +1000

    make the list items clickable

--- lib/widgets/books/collection_tile_list.dart
+++ lib/widgets/books/collection_tile_list.dart
@@ -1,51 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:paladin/repositories/collection_repository.dart';
+import 'package:paladin/widgets/home/fatal_error.dart';
 
 import '../../models/collection.dart';
 import '../../screens/book_list.dart';
 
 class CollectionTileList extends ConsumerWidget {
-  final ScrollController scrollController = ScrollController();
   final Collection collection;
 
-  CollectionTileList({super.key, required this.collection});
+  const CollectionTileList({super.key, required this.collection});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     var collectionListAsync = ref.watch(collectionRepositoryProvider(collection));
-    return collectionListAsync.when(error: (error, stackTrace) {
-      // TODO: better error message
-      return Text("It's a crime. You have no books!\n$stackTrace");
+    return collectionListAsync.when(error: (error, trace) {
+      return FatalError(error: "It's a crime; your book collection is empty!", trace: trace);
     }, loading: () {
       return const Center(child: CircularProgressIndicator());
     }, data: (List<Collection> collectionList) {
-      return SingleChildScrollView(
-          controller: scrollController,
-          child: ListView.builder(
+      return ListView.builder(
               itemCount: collectionList.length,
               itemBuilder: (context, index) {
-                return Container(
-                  color: index % 2 == 0 ? Colors.grey[50] : Colors.white,
-                  padding: const EdgeInsets.only(left: 8.0, top: 2.0, bottom: 2.0),
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: TextButton(
-                      onPressed: () => _navigateToCollection(context, collectionList[index]),
-                      style: ButtonStyle(
-                        backgroundColor: (index % 2 == 0)
-                            ? const WidgetStatePropertyAll(Colors.grey)
-                            : const WidgetStatePropertyAll(Colors.white),
-                        foregroundColor: const WidgetStatePropertyAll(Colors.black),
-                      ),
-                      child: Text(collectionList[index].toString(), style: Theme.of(context).textTheme.labelSmall),
-                    ),
+                return InkWell(
+                  onTap: () => _navigateToCollection(context, collectionList[index]),
+                  child: Container(
+                    color: index % 2 == 0 ? Colors.grey[50] : Colors.white,
+                    padding: const EdgeInsets.only(left: 8.0, top: 2.0, bottom: 2.0),
+                    child: Text(collectionList[index].toString(), style: Theme.of(context).textTheme.labelSmall),
                   ),
                 );
               },
               scrollDirection: Axis.vertical,
-              shrinkWrap: true),
-      );
+              shrinkWrap: true);
     });
   }
 
