1a291f330cae3d1b8176caeeafcb39461bd8507d
David Hobley <david.hobley@sharpblue.com.au>
2025-08-21 07:58:17 +1000

    build the widget correctly given the data is Async

--- lib/widgets/books/book_tile.dart
+++ lib/widgets/books/book_tile.dart
@@ -4,6 +4,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../models/book.dart';
 import '../../providers/cached_cover.dart';
 import '../../screens/backcover.dart';
+import '../home/fatal_error.dart';
 import '../menu/paladin_menu.dart';
 import 'authors.dart';
 import 'blurb.dart';
@@ -21,24 +22,29 @@ class BookTile extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     var coverAsync = ref.watch(cachedCoverProvider(book));
 
-    return InkWell(
-      onTap: () {
-        if (showMenu) {
-          book.readBook(context, ref);
-        } else {
-          Navigator.push(context, MaterialPageRoute(builder: (context) => BackCover(book: book)));
-        }
-      },
-      child: Row(
-        children: [
-          Container(
-            padding: const EdgeInsets.only(left: 6.0, right: 8.0, top: 4.0, bottom: 2.0),
-            child: coverAsync.value,
-          ),
-          Expanded(
-            child: Stack(
-              children: [
-                Center(
+    return coverAsync.when(error: (error, stackTrace) {
+      return FatalError(error: '${book.title}: ${error.toString()}', trace: stackTrace);
+    }, loading: () {
+      return const Center(child: CircularProgressIndicator());
+    }, data: (Image cover) {
+      return InkWell(
+        onTap: () {
+          if (showMenu) {
+            book.readBook(context, ref);
+          } else {
+            Navigator.push(context, MaterialPageRoute(builder: (context) => BackCover(book: book)));
+          }
+        },
+        child: Row(
+          children: [
+            Container(
+              padding: const EdgeInsets.only(left: 6.0, right: 8.0, top: 4.0, bottom: 2.0),
+              child: coverAsync.value,
+            ),
+            Expanded(
+              child: Stack(
+                children: [
+                  Center(
                     child: Column(
                       children: [
                         BookTitle(book: book,),
@@ -49,22 +55,23 @@ class BookTile extends ConsumerWidget {
                       ],
                     ),
                   ),
-                Align(
-                  alignment: Alignment.topRight,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      if (showMenu) PaladinMenu(),
-                      if (!showMenu && book.series != null) BookSeries(book: book),
-                      if (!showMenu) LastRead(book: book),
-                    ],
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        if (showMenu) PaladinMenu(),
+                        if (!showMenu && book.series != null) BookSeries(book: book),
+                        if (!showMenu) LastRead(book: book),
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
-      ),
-    );
+          ],
+        ),
+      );
+    });
   }
 }
