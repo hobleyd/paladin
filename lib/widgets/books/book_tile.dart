b8ee662bcbd752c7edd42d8c905bd1c379e418eb
David Hobley <david.hobley@sharpblue.com.au>
2025-08-27 12:42:04 +1000

    ensure we use the BackCover widget to display the read status correctly

--- lib/widgets/books/book_tile.dart
+++ lib/widgets/books/book_tile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:paladin/widgets/books/book_cover.dart';
 
 import '../../models/book.dart';
 import '../../providers/cached_cover.dart';
@@ -20,58 +21,50 @@ class BookTile extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    var coverAsync = ref.watch(cachedCoverProvider(book));
-
-    return coverAsync.when(error: (error, stackTrace) {
-      return FatalError(error: '${book.title}: ${error.toString()}', trace: stackTrace);
-    }, loading: () {
-      return const Center(child: CircularProgressIndicator());
-    }, data: (Image cover) {
-      return InkWell(
-        onTap: () {
-          if (showMenu) {
-            book.readBook(context, ref);
-          } else {
-            Navigator.push(context, MaterialPageRoute(builder: (context) => BackCover(book: book)));
-          }
-        },
-        child: Row(
-          children: [
-            Container(
-              padding: const EdgeInsets.only(left: 6.0, right: 8.0, top: 4.0, bottom: 2.0),
-              child: cover,
-            ),
-            Expanded(
-              child: Stack(
-                children: [
-                  Center(
-                    child: Column(
-                      children: [
-                        BookTitle(book: book,),
-                        if (showMenu) BookSeries(book: book),
-                        Authors(book: book),
-                        const SizedBox(height: 3),
-                        Blurb(book: book),
-                      ],
-                    ),
+    return InkWell(
+      onTap: () {
+        if (showMenu) {
+          book.readBook(context, ref);
+        } else {
+          Navigator.push(context, MaterialPageRoute(builder: (context) => BackCover(book: book)));
+        }
+      },
+      child: Row(
+        children: [
+          Container(
+            padding: const EdgeInsets.only(left: 6.0, right: 8.0, top: 4.0, bottom: 2.0),
+            child: BookCover(book: book),
+          ),
+          Expanded(
+            child: Stack(
+              children: [
+                Center(
+                  child: Column(
+                    children: [
+                      BookTitle(book: book,),
+                      if (showMenu) BookSeries(book: book),
+                      Authors(book: book),
+                      const SizedBox(height: 3),
+                      Blurb(book: book),
+                    ],
                   ),
-                  Align(
-                    alignment: Alignment.topRight,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        if (showMenu) PaladinMenu(),
-                        if (!showMenu && book.series != null) BookSeries(book: book),
-                        if (!showMenu) LastRead(book: book),
-                      ],
-                    ),
+                ),
+                Align(
+                  alignment: Alignment.topRight,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      if (showMenu) PaladinMenu(),
+                      if (!showMenu && book.series != null) BookSeries(book: book),
+                      if (!showMenu) LastRead(book: book),
+                    ],
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
-        ),
-      );
-    });
+          ),
+        ],
+      ),
+    );
   }
 }
