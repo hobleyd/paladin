647310899e7367d26d4ee87a54e6f73d7f24c958
David Hobley <david.hobley@sharpblue.com.au>
2025-09-16 20:26:36 +1000

    only display the first 25 books in each category

--- lib/widgets/books/book_table.dart
+++ lib/widgets/books/book_table.dart
@@ -13,27 +13,31 @@ class BookTable extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return FutureBuilder(
-        future: future,
-        builder: (BuildContext ctx, AsyncSnapshot<CalibreBookCount> bookCount) {
-          int count = bookCount.data == null ? 0 : bookCount.data!.count;
-          return Column(
-            children: [
-              Text('There are $count $label', style: Theme.of(context).textTheme.labelMedium),
-              const Divider(color: Colors.black, thickness: 1),
-              if (bookCount.data != null) Table(
-                children: [
-                  for (var book in bookCount.data!.books) TableRow(
-                    decoration: BoxDecoration(color: bookCount.data!.books.indexOf(book) % 2 == 0 ? Colors.grey.shade300 : Colors.grey.shade200,),
+      future: future,
+      builder: (BuildContext ctx, AsyncSnapshot<CalibreBookCount> bookCount) {
+        int count = bookCount.data == null ? 0 : bookCount.data!.count;
+        return Column(
+          children: [
+            Text('There are $count $label', style: Theme.of(context).textTheme.labelMedium),
+            const Divider(color: Colors.black, thickness: 1),
+            Table(
+              children: [
+                // TODO: I'd prefer this to be scrollable, but I can't figure that out! So manage it manually.
+                for (int i = 0; i < (count > 25 ? 25 : count); i++)
+                  TableRow(
+                    decoration: BoxDecoration(color: i % 2 == 0 ? Colors.grey.shade300 : Colors.grey.shade200,),
                     children: [
-                      TableCell(child: Text(book.title, style: Theme.of(context).textTheme.bodySmall)),
-                      TableCell(child: Text(book.author, style: Theme.of(context).textTheme.bodySmall)),
-                      TableCell(child: Text(getFormattedDateTime(book.lastModified), style: Theme.of(context).textTheme.bodySmall)),
+                      TableCell(child: Text(bookCount.data!.books[i].title, style: Theme.of(context).textTheme.bodySmall)),
+                      TableCell(child: Text(bookCount.data!.books[i].author, style: Theme.of(context).textTheme.bodySmall)),
+                      TableCell(child: Text(getFormattedDateTime(bookCount.data!.books[i].lastModified), style: Theme.of(context).textTheme.bodySmall)),
                     ],
                   ),
-                ],
-              ),
-            ],
-          );
-        });
+              ],
+            ),
+            if (count > 25) Text('...'),
+          ],
+        );
+      },
+    );
   }
 }
