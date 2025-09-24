19ed2c57a59f846d19b67087120e916ea363adac
David Hobley <david.hobley@sharpblue.com.au>
2025-09-24 17:34:15 +1000

    show circular indicator when downloading

--- lib/widgets/settings/paladin_update.dart
+++ lib/widgets/settings/paladin_update.dart
@@ -7,37 +7,56 @@ import 'package:path_provider/path_provider.dart';
 import '../../models/version_check.dart';
 import '../../providers/update.dart';
 
-class PaladinUpdate extends ConsumerWidget {
-  const PaladinUpdate({super.key});
+class PaladinUpdate extends ConsumerStatefulWidget {
+  const PaladinUpdate({super.key,});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<PaladinUpdate> createState() => _PaladinUpdate();
+}
+
+class _PaladinUpdate extends ConsumerState<PaladinUpdate> {
+  bool downloading = false;
+
+  @override
+  Widget build(BuildContext context) {
     VersionCheck? versions = ref.watch(updateProvider).value;
 
     final String noUpdateLabel = 'There are no updates for Paladin as at this time.';
-    return versions == null
-        ? Padding(padding: EdgeInsetsGeometry.only(top: 30), child: Text(noUpdateLabel, style: Theme.of(context).textTheme.labelMedium))
-        : Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(padding: EdgeInsetsGeometry.only(top: 30), child: Text('Installed Version: ${versions.currentVersion}', style: Theme.of(context).textTheme.bodyMedium)),
-            Text('Current Version: ${versions.newVersion}', style: Theme.of(context).textTheme.bodyMedium),
-            if (versions.hasUpdate)
-              Padding(padding: const EdgeInsets.only(top: 10), child: IconButton(icon: const Icon(Icons.download), onPressed: () => _download(ref, versions.downloadUrl, versions.downloadPackage))),
-            if (!versions.hasUpdate)
-              Padding(padding: const EdgeInsets.only(top: 10), child: IconButton(icon: const Icon(Icons.refresh), onPressed: () => ref.read(updateProvider.notifier).checkVersion())),
-          ]
-    );
+    return downloading == true
+        ? CircularProgressIndicator()
+        : versions == null
+          ? Padding(padding: EdgeInsetsGeometry.only(top: 30), child: Text(noUpdateLabel, style: Theme.of(context).textTheme.labelMedium))
+          : Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(padding: EdgeInsetsGeometry.only(top: 30), child: Text('Installed Version: ${versions.currentVersion}', style: Theme.of(context).textTheme.bodyMedium)),
+                Text('Current Version: ${versions.newVersion}', style: Theme.of(context).textTheme.bodyMedium),
+                if (versions.hasUpdate)
+                  Padding(padding: const EdgeInsets.only(top: 10),
+                      child: IconButton(icon: const Icon(Icons.download), onPressed: () => _download(ref, versions.downloadUrl, versions.downloadPackage))),
+                if (!versions.hasUpdate)
+                  Padding(padding: const EdgeInsets.only(top: 10),
+                      child: IconButton(icon: const Icon(Icons.refresh), onPressed: () => ref.read(updateProvider.notifier).checkVersion())),
+              ]
+            );
   }
 
   // TODO: UX affordance for when we are downloading.
   Future<void> _download(WidgetRef ref, String url, String package) async {
+    setState(() {
+      downloading = true;
+    });
+
     String apkPath = "${(await getTemporaryDirectory()).path}/$package}";
     await Dio().download(url, apkPath);
+
+    setState(() {
+      downloading = false;
+    });
+
     int? statusCode = await AndroidPackageInstaller.installApk(apkFilePath: apkPath);
     if (statusCode != null) {
       PackageInstallerStatus installationStatus = PackageInstallerStatus.byCode(statusCode);
-      debugPrint(installationStatus.name);
     }
   }
 }
