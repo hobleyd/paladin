cb84a7c4f9b3a4316d88e157787f10bfc25ad5bb
David Hobley <david.hobley@sharpblue.com.au>
2025-09-25 17:49:30 +1000

    remove an undded call to the bookProvider.getBook()

--- lib/widgets/books/book_tile.dart
+++ lib/widgets/books/book_tile.dart
@@ -23,7 +23,6 @@ class BookTile extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     Book? book = ref.watch(bookProviderProvider(bookUuid));
     if (book == null) {
-      ref.read(bookProviderProvider(bookUuid).notifier).getBook();
       return const Text('');
     }
 
